5a8771c75eb6db1f9d52cb9058fbf159bd13abfa
mikecode <102126994+miguelbtcode@users.noreply.github.com>
2024-03-16 00:18:59 -0500

    Add security options with JwtBearer tokens and CorsPolicy Configurations

--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -1,8 +1,63 @@
+using System.Text;
+using Ecommerce.Domain;
+using Ecommerce.Persistence;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EcommerceDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"),
+        b => b.MigrationsAssembly(typeof(EcommerceDbContext).Assembly.FullName)
+        )
+);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => 
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
+IdentityBuilder identityBuilder = builder.Services.AddIdentityCore<Usuario>();
+identityBuilder = new IdentityBuilder(identityBuilder.UserType, identityBuilder.Services);
+identityBuilder.AddRoles<IdentityRole>().AddDefaultTokenProviders();
+identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<Usuario, IdentityRole>>();
+
+identityBuilder.AddEntityFrameworkStores<EcommerceDbContext>();
+identityBuilder.AddSignInManager<SignInManager<Usuario>>();
+
+builder.Services.TryAddSingleton<ISystemClock, SystemClock>();
+
+
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:key"]!));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt => 
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = key,
+        ValidateAudience = false,
+        ValidateIssuer = false
+    };
+});
+
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", builder => builder
+                                .AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,9 +71,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
