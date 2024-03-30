d5da57f341363f82bcf912d482688ea8a342e8b3
mikecode <102126994+miguelbtcode@users.noreply.github.com>
2024-03-30 09:58:25 -0500

    Add command and hanler by login

---
+++ backend/src/Core/Ecommerce.Application/Features/Addresses/Vms/AddressVm.cs
@@ -0,0 +1,10 @@
+namespace Ecommerce.Application.Features.Addresses.Vms;
+
+public class AddressVm
+{
+    public string? Direccion { get; set; }
+    public string? Ciudad { get; set; }
+    public string? Departamento { get; set; }
+    public string? CodigoPostal { get; set; }
+    public string? Pais { get; set; }
+}
