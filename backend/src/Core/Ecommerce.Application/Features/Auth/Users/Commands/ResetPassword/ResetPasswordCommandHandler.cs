142909f06b2348a6ba65067fd6d145b6220a9638
mikecode <102126994+miguelbtcode@users.noreply.github.com>
2024-03-31 14:29:27 -0500

    Add command: Reset Password

---
+++ backend/src/Core/Ecommerce.Application/Features/Auth/Users/Commands/ResetPassword/ResetPasswordCommandHandler.cs
@@ -0,0 +1,45 @@
+using Ecommerce.Application.Exceptions;
+using Ecommerce.Application.Identity;
+using Ecommerce.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ecommerce.Application.Features.Auth.Users.Commands.ResetPassword;
+
+public class ResetPasswordCommandHandler : IRequestHandler<ResetPasswordCommand>
+{
+    private readonly UserManager<Usuario> _userManager;
+    private readonly IAuthService _authService;
+
+    public ResetPasswordCommandHandler(UserManager<Usuario> userManager, IAuthService authService)
+    {
+        _userManager = userManager;
+        _authService = authService;
+    }
+
+    public async Task<Unit> Handle(ResetPasswordCommand request, CancellationToken cancellationToken)
+    {
+        var updateUser = await _userManager.FindByNameAsync(_authService.GetSessionUser()) ?? throw new BadRequestException("El usuario no existe");
+
+        var resultValidateOldPassword = _userManager.PasswordHasher.VerifyHashedPassword(updateUser, 
+                                                                                         updateUser.PasswordHash!, 
+                                                                                         request.OldPassword!);
+
+        if (resultValidateOldPassword != PasswordVerificationResult.Success)
+        {
+            throw new BadRequestException("El actual password ingresado es erroneo");
+        }
+
+        var hashedNewPassword = _userManager.PasswordHasher.HashPassword(updateUser, request.NewPassword!);
+        updateUser.PasswordHash = hashedNewPassword;
+
+        var resultUpdate = await _userManager.UpdateAsync(updateUser);
+
+        if (!resultUpdate.Succeeded)
+        {
+            throw new Exception("No se pudo resetear el password");
+        }
+
+        return Unit.Value;
+    }
+}
